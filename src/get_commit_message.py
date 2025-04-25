2c062ec7a6aa3862007d4116c110d95748b81160
Aurum <apppokket@gmail.com>
2025-04-25 14:16:08 +0330

    Add files via upload

---
+++ src/get_commit_message.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import pytz
+import jdatetime
+
+# Set timezone to Tehran
+tehran_time = datetime.now(pytz.timezone("Asia/Tehran"))
+shamsi_date = jdatetime.datetime.fromgregorian(datetime=tehran_time).strftime("%Y-%m-%d")
+time_str = tehran_time.strftime("%H:%M:%S")
+
+commit_message = f"Auto: Data update {shamsi_date.replace('-', '/')} {time_str}"
+print(commit_message)
